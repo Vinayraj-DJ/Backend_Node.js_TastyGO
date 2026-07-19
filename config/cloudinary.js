6382c09c57610e0a0a3b0641041b087a94368079
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2026-07-19 17:41:38 +0530

    modify cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,4 +1,6 @@
 const { v2: cloudinary } = require("cloudinary");
+const dotenv = require("dotenv");
+dotenv.config();
 
 console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
 console.log("API Key:", process.env.CLOUDINARY_API_KEY);
