419183fb7f2a16c9d7d22a7fd5d4c26344fe1caf
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2026-07-19 18:18:52 +0530

    modify uploads

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,46 +1,21 @@
-// const multer = require("multer");
-// const path=require("path")
-// const storage = multer.diskStorage({
-
-//     destination: function(req,file,cb){
-
-//         cb(null,"uploads");
-
-//     },
-
-//     filename:function(req,file,cb){
-
-//         cb(null,Date.now()+"-"+path.extname(file.originalname));
-
-//     }
-
-// });
-
-// const upload = multer({storage});
-
-// module.exports = upload;
-
-
-
-
-
-
-
-
-
-
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: async (req, file) => ({
-        folder: "TastyGo",
-        allowed_formats: ["jpg", "jpeg", "png", "webp"]
-    })
+  cloudinary: cloudinary,
+  params: {
+    folder: "TastyGo",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    resource_type: "image",
+  },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
 
 module.exports = upload;
